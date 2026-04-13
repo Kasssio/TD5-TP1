f14b6a9b63ef0e0eeffeeec4f975580f34eeb031
Lucas Estrada <101399158+Kasssio@users.noreply.github.com>
2026-04-13 17:16:54 -0300

    added some images

--- source/FuerzaBruta.cpp
+++ source/FuerzaBruta.cpp
@@ -8,23 +8,22 @@ int m;
 double minEnergy = __DBL_MAX__;
 void FuerzaBruta(const vector<vector<double>>& energia, int i, int j, pair<vector<int>,double> currSeam){ // O(3^n) 
     if(j < 0 || j >= m) return; // Chequeo fuera de rango. O(1).
-    pair<vector<int>, double> temp = currSeam; // La costura que nos queda desde donde estamos parados. O(1).
-    temp.first.push_back(j); // O(1) amortizado
-    temp.second += energia[i][j]; // O(1)
+    currSeam.first.push_back(j); // O(1) amortizado
+    currSeam.second += energia[i][j]; // O(1)
     if(i == n-1){ // Caso base, llegamos al final. O(1).
-        if(temp.second < minEnergy) { // Reemplazamos el mejor si encontramos un mejor resultado. O(1).
-            minEnergy = temp.second; // O(1)
-            best = temp.first; // O(1)
+        if(currSeam.second < minEnergy) { // Reemplazamos el mejor si encontramos un mejor resultado. O(1).
+            minEnergy = currSeam.second; // O(1)
+            best = currSeam.first; // O(1)
         }
         return; // O(1)
     }
     for(int k=-1; k<=1;k++) { // Recorremos energia[i+1][j-1], energia[i+1][j] y energia[i+1][j+1] recursivamente. O(1).
-        FuerzaBruta(energia, i+1,j+k,temp);
+        FuerzaBruta(energia, i+1,j+k,currSeam);
     }
 }
 std::vector<int> encontrarSeamFuerzaBruta(const std::vector<std::vector<double>>& energia) { // Complejidad final: O(m*3^n).
     if (energia.empty()) return {};
-    // Reseteamos variables globales.
+    // Definimos variables globales.
     n = energia.size();
     m = energia[0].size();
     minEnergy = __DBL_MAX__;
